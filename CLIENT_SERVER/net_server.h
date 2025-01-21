91b54da265883888a8fe93b5929addcb231b748d
Kfir wolf <kfirwolf@gmail.com>
2025-01-21 20:34:01 +0200

    update server header

--- CLIENT_SERVER/net_server.h
+++ CLIENT_SERVER/net_server.h
@@ -1,9 +1,13 @@
 #include <stdint.h>
 #include <string.h>
 
-int net_server_create();
-int net_server_start();
+typedef struct {
+    char *ip;
+    int port;
+    int sock_fd;
+    int time_out;
+} cfg_t;    
+
+int net_server_start(cfg_t *cfg);
 int net_server_stop();
 int net_server_destroy();
-int net_server_bind(int socketFD);
-int net_server_unbind(int socketFD);
