e3c0e645f70cc3f433eb9dbbb9bfe674a98b23b8
kfirwolf <kfirwolf@gmail.com>
2025-01-22 14:22:56 +0200

    delete the client_server from this branch

--- CLIENT_SERVER/net_client.h
+++
@@ -1,9 +0,0 @@
-#include <net_common.h>
-
-int clientCreateSocket(char *ip, int port);
-
-int clientSend(int socketfd, char *req, short reqLen);
-
-int clientRecive(int socketfd, char *resp, short respLen);
-
-int clientClose();
--- CLIENT_SERVER/net_server.h
+++
@@ -1,5 +0,0 @@
-#include <net_common.h>
-
-int serverBind(int socketFD);
-
-int serverRecive();
