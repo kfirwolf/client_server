ba1eec51adbf3ec084f3576b6e4dc7c03230005e
kfirwolf <kfirwolf@gmail.com>
2025-01-22 12:38:57 +0200

    added data handler callback

--- CLIENT_SERVER/net_client.h
+++ CLIENT_SERVER/net_client.h
@@ -10,10 +10,10 @@ typedef struct {
 
 typedef struct {
     uint8_t *data;
-    int data_len;
+    size_t data_len;
 } data_t;
 
-int net_client_create(cfg_t *cfg);
-int net_client_send(cfg_t *cfg, const data_t *data_s);
-int net_client_recive(cfg_t *cfg, data_t *data_s);
-int net_client_destroy(cfg_t *cfg);
+int net_client_create(cfg_t *client_cfg);
+int net_client_send(cfg_t *client_cfg, const data_t *data_s);
+int net_client_recive(cfg_t *client_cfg, data_t *data_s);
+int net_client_destroy(cfg_t *client_cfg);
--- CLIENT_SERVER/net_server.h
+++ CLIENT_SERVER/net_server.h
@@ -1,13 +1,20 @@
 #include <stdint.h>
 #include <string.h>
+#include <unistd.h>
+
+typedef struct sockaddr sockaddr_t; //defined it instead of using #include <netinet/in.h> and using sockaddr_in (we will use the correct one in the c file)
+typedef unsigned int socklen_t; //defined it instead of #include <sys/socket.h> here
 
 typedef struct {
     char *ip;
-    int port;
+    uint16_t local_port;
     int sock_fd;
     int time_out;
-} cfg_t;    
+} cfg_t;
 
-int net_server_start(cfg_t *cfg);
-int net_server_stop();
-int net_server_destroy();
+
+typedef void (*data_handler_t)(const uint8_t *data, size_t data_len, const struct sockaddr_t *src_addr, socklen_t addrlen); //callback for continues retrive of data
+
+
+int net_server_start(cfg_t *server_cfg, data_handler_t *d_handler);
+int net_server_stop(cfg_t *server_cfg);
