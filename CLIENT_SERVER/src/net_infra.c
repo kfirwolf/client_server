a32d736a4573c59acad763b11518d800e696a5cb
kfirwolf <kfirwolf@gmail.com>
2025-02-11 16:00:13 +0200

    implementation and API for memory manager interface

--- CLIENT_SERVER/src/net_infra.c
+++ CLIENT_SERVER/src/net_infra.c
@@ -2,16 +2,27 @@
 #include <stdlib.h>
 #include "net_infra.h"
 
+static log_level_t current_log_level = LOG_INFO;
+
+void set_log_level(log_level_t log_level) {
+    current_log_level = log_level;
+}
+
 void net_infra_log(log_level_t level, const char *file, int line, const char *func, const char *format, ...) {
+
+    if (level < current_log_level ) {
+        return;
+    }
+
     va_list args;
     va_start(args, format);
 
     const char *level_str;
     switch (level) {
+        case LOG_DEBUG:   level_str = "DEBUG";   break;        
         case LOG_INFO:    level_str = "INFO";    break;
         case LOG_WARNING: level_str = "WARNING"; break;
         case LOG_ERROR:   level_str = "ERROR";   break;
-        case LOG_DEBUG:   level_str = "DEBUG";   break;
         default:          level_str = "UNKNOWN"; break;
     }
 
